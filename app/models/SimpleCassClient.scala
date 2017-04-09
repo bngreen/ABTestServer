99d72cc72c85797888355e04545537550e939f0b
Bruno Green <bruno.green@gmail.com>
2017-04-09 07:45:20 -0300

    Added event schema

--- app/models/SimpleCassClient.scala
+++ app/models/SimpleCassClient.scala
@@ -1,3 +1,22 @@
+/*
+Copyright (C) 2017  Bruno Naspolini Green. All rights reserved.
+
+This file is part of ABTestServer.
+
+ABTestServer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ABTestServer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ABTestServer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package models
 
 import javax.inject.{Inject, Singleton}
@@ -39,6 +58,21 @@ class SimpleCassClient @Inject()(configuration: Configuration) {
       )
       session.execute(
         """CREATE INDEX IF NOT EXISTS variation_experiment_id ON abtest.variation (experiment_id);"""
+      )
+      session.execute(
+        """CREATE TABLE IF NOT EXISTS abtest.event (
+          id uuid PRIMARY KEY,
+          time timestamp,
+          userid text,
+          name text,
+          metrics map<text, text>,
+          experiments map<text, text>
+          );""")
+      session.execute(
+        """CREATE INDEX IF NOT EXISTS event_metric_key ON abtest.event (KEYS(metrics));"""
+      )
+      session.execute(
+        """CREATE INDEX IF NOT EXISTS event_experiment_key ON abtest.event (KEYS(experiments));"""
       )
     }
   createSchema()
